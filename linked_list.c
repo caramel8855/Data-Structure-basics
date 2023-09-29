21f39e5c84b94fea3fa962c5446c932012831ac2
caramel8855 <134868651+caramel8855@users.noreply.github.com>
2023-09-29 20:57:05 +0600

    an example of linked list

--- linked_list.c
+++ linked_list.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//create a struct 
 struct node{
     int data;
     struct node *link;
 };
 
 int main(){
+    //declare pointers of struct node
     struct node *head;
     struct node *second;
     struct node *third;
     struct node *forth;
- 
+
+    //allocate the memories
     head = (struct node *) malloc(sizeof(struct node));
     second = (struct node *) malloc(sizeof(struct node));
     third = (struct node *) malloc(sizeof(struct node));
-   
+
+    //start creating and linking 
     head->data = 7;
     head->link = second;
     second->data = 73;
